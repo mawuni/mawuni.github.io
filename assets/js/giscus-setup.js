57866b6386621a19f53424347467e6efdb886952
mawuni <123791718+mawuni@users.noreply.github.com>
2026-02-12 15:26:39 +0000

    Deploying to gh-pages from @ mawuni/mawuni.test.io@519b6481289a2e2ece21bf9a8b8c01eac6441010 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mawuni/mawuni.github.io",
+    "data-repo": "mawuni/mawuni.test.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
